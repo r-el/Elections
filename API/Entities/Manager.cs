bb2206e5b2caa40ddf76e4987239da5ee7947147
rel <arieltanami122@gmail.com>
2022-02-22 03:17:55 +0200

    Add Manager Entity

---
+++ API/Entities/Manager.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+    public class Manager
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "שם מלא")]
+        [Required(ErrorMessage = "הזן שם מלא")]
+        public string FullName { get; set; }
+
+        [Display(Name = "מספר טלפון")]
+        [Required(ErrorMessage = "הזן מספר טלפון")]
+        public string Phone { get; set; }
+
+        [Display(Name = "כתובת מייל")]
+        [Required(ErrorMessage = "הזן כתובת מייל")]
+        [EmailAddress(ErrorMessage = "נא הכנס כתובת מייל נכונה")]
+        public string Mail { get; set; }
+
+        [Display(Name = "סיסמא")]
+        [Required(ErrorMessage = "הזן סיסמא")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
